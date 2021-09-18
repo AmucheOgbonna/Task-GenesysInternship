26101a5e8f79e76f849d15648b9eb74c24a6aefc
AmucheOgbonna <amuche247cynthia@gmail.com>
2021-09-18 13:10:22 +0100

    updated

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
 const list = document.body;
 const task = document.getElementById("text");
-const submit = document.getElementById("submit");
-
-
-
-submit.addEventListener("click", onClicking);
-
-/* function onClicking() {
-
-    // first validate that a task is entered...
-    if (task) {
-
-    }
-} */
 
 
 //to check form
 const form = document.getElementById("form");
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-    const value = form.querySelector("input[type='text']").value;
-    console.log(value);
+    onClicking();
+
 });
+
+//on form submission
+const submit = document.getElementById("submit");
+function onClicking() {
+    var updatedTask = document.getElementsByTagName('tbody')[0];
+    const uploadTask = document.getElementById("uploadTask");
+    if (uploadTask.innerText === '') {
+        var styles = ' table { visibility: visible;}';
+        var stylesheet = document.createElement('style');
+        stylesheet.type = "text/css";
+        stylesheet.innerText = styles;
+        document.head.appendChild(stylesheet);
+        uploadTask.innerHTML = task.value;
+    } else {
+        var newRow = updatedTask.insertRow(updatedTask.length);
+        newRow.innerHTML = `<tr id="data">
+        <td scope="col" id="uploadTask">' ${task.value} </td>
+        <td scope="col" id="delete"><i class="bi bi-x-circle"></i></td>        
+        <td scope="col" id="check"><i class="bi bi-check-circle"></i></td>
+        <td scope="col" d="edit"><i class="bi bi-pencil-square"></i></td>
+        </tr>`;
+        // newRow.innerHTML = task.value;
+    }
+}
+//To edit
+var editData = document.getElementById('edit');
+editData.addEventListener('click', toEdit());
+function toEdit() {
+    var logg = editData.parentElement.nodeName;
+    console.log(logg);
+}
